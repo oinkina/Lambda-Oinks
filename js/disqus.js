1d38f60d87a1046f265b3554bbb33edffd0abaff
oinkina <lambdaoinks@gmail.com>
2014-03-25 02:57:21 -0700

    put blog post title inside first column

---
+++ js/disqus.js
@@ -0,0 +1,15 @@
+(function() {
+        var SHORTNAME = 'example'; // Your website's shortname on Disqus
+        var dsq = document.createElement('script'); dsq.type =
+'text/javascript'; dsq.async = true;
+        dsq.src = 'http://disqus.com/forums/gwern/embed.js';
+        (document.getElementsByTagName('head')[0] ||
+document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+        var disqus_shortname = 'gwern';
+        (function () {
+         var s = document.createElement('script'); s.async = true;
+         s.src = 'http://disqus.com/forums/gwern/count.js';
+         (document.getElementsByTagName('HEAD')[0] ||
+document.getElementsByTagName('BODY')[0]).appendChild(s);
+       }());
