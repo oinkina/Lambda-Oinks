4dd860fab5f862dca03666d05f608b4cd782156f
oinkina <lambdaoinks@gmail.com>
2014-04-05 17:22:33 -0700

    updated newpost.sh

--- newpost.sh
+++ newpost.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # Requires hub: brew install hub; alias git=hub
-# to run: ./newpost [REPO NAME] [POST TITLE] [LOCAL DIR NAME]
+# to run: ./newpost [REPO NAME] [POST TITLE]
+DIR=`date +%F`-$1
+
 hub create lambda-oinks/$1
+
 (cd ~/lambda-oinks/posts
 git clone lambda-oinks/$1
+
 (cd $1
 echo "---" >> index.md
 echo "title: $2" >> index.md
@@ -15,10 +19,15 @@ echo "---" >> index.md
 git add index.md
 git commit -a -m "initialized with template for index.md"
 )
+
 rm -rf ~/lambda-oinks/posts/$1
-git submodule add lambda-oinks/$1 $3
-git commit -a -m "added new post submodule: $1"
+git submodule add lambda-oinks/$1 $DIR
+git commit -a -m "added new post submodule: $DIR"
 git push origin master
 )
+
 (cd ~/lambda-oinks
 git add .
+git commit -a -m "added new post submodule within posts/: $DIR"
+git push origin master
+)
